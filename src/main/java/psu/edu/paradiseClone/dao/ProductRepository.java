821ec9d1f08b685e57c7678110603b5264bb9c02
Luke Hanrahan <lhanrahan2@gmail.com>
2024-04-21 00:08:38 -0400

    search for previous commit

--- src/main/java/psu/edu/paradiseClone/dao/ProductRepository.java
+++ src/main/java/psu/edu/paradiseClone/dao/ProductRepository.java
@@ -1,5 +1,7 @@
 package psu.edu.paradiseClone.dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import psu.edu.paradiseClone.entity.Product;
@@ -8,6 +10,8 @@ public interface ProductRepository extends JpaRepository<Product, Integer> {
 	
 	public Product findBySku(String sku);
 	public Product findById(int id);
+	public List<Product> findByNameLike(String name);
+	public List<Product> findByNameContaining(String name);
 
 
 }
